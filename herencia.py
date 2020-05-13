01195667766474ca29d2d36e0cbc51fdeda376d0
RETSIEMRED <freivpbvf@gmail.com>
2020-05-13 17:12:55 -0500

    Creé codigo de herencia con rectángulo y cuadrado

---
+++ herencia.py
@@ -0,0 +1,20 @@
+ # Esta es mi súper clase
+class Rectangulo:
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def area(self):
+        return self.base * self.altura
+# Esta es mi clase, en python usas como super clase metiendolo en los parentesis
+#La clase cuadrado, extiende al rectanulo
+class Cuadrado(Rectangulo):   
+    def __init__ (self, lado):
+        super().__init__(lado, lado)     #Super: Obtiene una referencia directa de la superclase
+
+if __name__ == '__main__':
+    rectangulo = Rectangulo(base = 3, altura = 4)
+    print(f'El área del rectángulo es {rectangulo.area()}')
+
+    cuadrado = Cuadrado(lado = 5)
+    print(f'El área del cuadrado es: {cuadrado.area()}')
