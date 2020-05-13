d4211cfa84c3ce9e50e9faef554df5b3db97dc40
RETSIEMRED <freivpbvf@gmail.com>
2020-05-13 17:31:50 -0500

    Agregué una subclase llamada triángulo

--- herencia.py
+++ herencia.py
@@ -6,11 +6,20 @@ class Rectangulo:
 
     def area(self):
         return self.base * self.altura
+
 # Esta es mi clase, en python usas como super clase metiendolo en los parentesis
 #La clase cuadrado, extiende al rectanulo
+
 class Cuadrado(Rectangulo):   
     def __init__ (self, lado):
-        super().__init__(lado, lado)     #Super: Obtiene una referencia directa de la superclase
+        super().__init__(lado, lado )     #Super: Obtiene una referencia directa de la superclase
+
+
+# ---------- Creo otra clase    -----
+
+class Triangulo(Rectangulo):
+    def __init__ (self, base, altura): 
+        super().__init__(base, altura)
 
 if __name__ == '__main__':
     rectangulo = Rectangulo(base = 3, altura = 4)
@@ -18,3 +27,6 @@ if __name__ == '__main__':
 
     cuadrado = Cuadrado(lado = 5)
     print(f'El área del cuadrado es: {cuadrado.area()}')
+
+    triangulo = Triangulo(base =2, altura = 2)
+    print(f'El área del triangulo es : {triangulo.area() // 2}')
